a91ddd841d9d7baff96e37fbaad90afa4ab2f5be
vasilki <lukanikhin@mail.ru>
2018-09-22 16:52:41 +0300

    fix warnings in dwt

--- dwt_stm32_delay/dwt_stm32_delay.c
+++ dwt_stm32_delay/dwt_stm32_delay.c
@@ -49,23 +49,4 @@ uint32_t DWT_Delay_Init(void)
   return loc_B_WasInitialized;
 }
 
-
-/**
- * @brief  This function provides a delay (in microseconds)
- * @param  microseconds: delay in microseconds
- */
-inline void DWT_Delay_us(volatile uint32_t microseconds)
-{
-  uint32_t clk_cycle_start = DWT->CYCCNT;
-
-  /* Go to number of cycles for system */
-  microseconds *= (HAL_RCC_GetHCLKFreq() / 1000000);
-
-  /* Delay till end */
-  while ((DWT->CYCCNT - clk_cycle_start) < microseconds);
-
-  return;
-}
-
-
 /* Use DWT_Delay_Init (); and DWT_Delay_us (microseconds) in the main */
--- dwt_stm32_delay/dwt_stm32_delay.h
+++ dwt_stm32_delay/dwt_stm32_delay.h
@@ -22,7 +22,18 @@ uint32_t DWT_Delay_Init(void);
  * @brief  This function provides a delay (in microseconds)
  * @param  microseconds: delay in microseconds
  */
-inline void DWT_Delay_us(volatile uint32_t microseconds);
+inline void DWT_Delay_us(volatile uint32_t microseconds)
+{
+  uint32_t clk_cycle_start = DWT->CYCCNT;
+
+  /* Go to number of cycles for system */
+  microseconds *= (HAL_RCC_GetHCLKFreq() / 1000000);
+
+  /* Delay till end */
+  while ((DWT->CYCCNT - clk_cycle_start) < microseconds);
+
+  return;
+}
 
 #ifdef __cplusplus
 }
